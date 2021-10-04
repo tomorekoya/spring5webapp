d31a0939995bacf64e6aaca09f6ca21710ef8ad0
Tomiwa Orekoya <orekoyatomiwa@gmail.com>
2021-10-04 15:22:22 -0400

    18 - Publisher Relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -24,29 +24,39 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
+
+        Publisher harperCollins = new Publisher("Harper Collins", "195 Broadway", "New York",
+                "NY", "10007");
+
+        publisherRepository.save(harperCollins);
+        System.out.println("Number of publishers: " + publisherRepository.count());
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        
+        ddd.setPublisher(harperCollins);
+        harperCollins.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(harperCollins);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development Without EJB", "432432432");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(harperCollins);
+        harperCollins.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher harperCollins = new Publisher("Harper Collins", "195 Broadway", "New York",
-                "NY", "10007");
-
         publisherRepository.save(harperCollins);
 
-        System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
-        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Publisher Number of Books: " + harperCollins.getBooks().size());
     }
 }
